7ec20fbd6d1852c741963ef323be40c9fb284c97
threalwinky <voquangvu09112006@gmail.com>
2023-07-09 00:16:48 +0700

    upload files

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from flask import Flask, redirect, url_for, render_template, request, session, j
 from datetime import timedelta
 from flask.helpers import flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, send
 from os import path
+import requests
+import json
 
 #Initing app and database connection
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "thanhdz"
+app.config["SECRET_KEY"] = "2b3ifbf302f9nc1j2po1jewkajsd"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=10)
 db.init_app(app)
+socketio = SocketIO(app, cors_allowed_origin="*")
 
 #Class of database 
 class User(db.Model):
@@ -22,6 +26,20 @@ class User(db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+def get_message(message):
+    headers = {
+        'Content-Type':'application/x-www-form-urlencoded',
+    }
+    API_ENDPOINT = 'https://api.simsimi.vn/v1/simtalk'
+    data = {
+        'text' : message,
+        'lc' : 'vn',
+        'key' : ''
+    }
+    res = requests.post(url = API_ENDPOINT, headers=headers, data=data)
+    json_text = json.loads(res.text)
+    return json_text['message']
 
 #Home Page
 @app.route('/', methods=["POST", "GET"])
@@ -88,6 +106,10 @@ def user_list():
     users = db.session.execute(db.select(User).order_by(User.username)).scalars()
     return render_template("list.html", users=users)
 
+@app.route("/about")
+def about_us():
+    return render_template('about.html')
+
 #Logging out in dashboard page
 @app.route('/logout')
 def log_out():
@@ -104,16 +126,20 @@ def dash_board():
         return redirect(url_for("log_in"))
 
 #AI chatbot Page
+@socketio.on('message')
+def handle_message(message):
+    print('Receive message : ' + message)
+    # print(get_message(message))
+    send(message, broadcast=True)
+    send(get_message(message), broadcast=True)
 @app.route('/app')
 def aichat():
     return render_template('chat.html')
 
-
-
 #Profile Page
-# @app.route('/profile')
-# def profile():
-#     return render_template('profile.html')
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 #Running website
 if __name__ == "__main__":
